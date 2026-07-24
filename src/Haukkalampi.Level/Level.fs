983cda8cbddab1f119e0b955f31f3f540fba08ba
Juuz <6596629+Juuxel@users.noreply.github.com>
2026-07-24 16:00:56 +0300

    Add MPL license

--- src/Haukkalampi.Level/Level.fs
+++ src/Haukkalampi.Level/Level.fs
@@ -1,4 +1,8 @@
-﻿namespace Haukkalampi.Level
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace Haukkalampi.Level
 
 open Haukkalampi.Core.Math
 open Haukkalampi.Player
--- src/Haukkalampi.Server/Program.fs
+++ src/Haukkalampi.Server/Program.fs
@@ -1,4 +1,8 @@
-﻿open Haukkalampi.Core.Io
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+open Haukkalampi.Core.Io
 open Haukkalampi.Core.Math
 open Haukkalampi.Core.Nbt
 open Haukkalampi.Level
